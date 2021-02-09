81c6ebd10ba2c4dbd58136eca16955a7067a1fb4
Jacob Olness <jacobolness@Jacobs-MBP.lan>
2021-02-09 16:26:54 -0700

    arrayList constructed

--- src/animal/Main.java
+++ src/animal/Main.java
@@ -2,8 +2,31 @@ package animal;
 
 public class Main
 {
-	public static void main(String[] arg)
+	
+	public static void main(String[] args)
 	{
+		System.out.println("This is a test...Yes... This is a test");
 		
+// Mammals
+		Mammals panda = new Mammals("Panda", 1869);
+		Mammals zebra = new Mammals("Zebra", 1778);
+		Mammals koala = new Mammals("Koala", 1816);
+		Mammals sloth = new Mammals("Sloth", 1804);
+		Mammals armadillo = new Mammals("Armadillo", 1758);
+		Mammals racoon = new Mammals("Raccoon", 1758);
+		Mammals bigfoot = new Mammals("Bigfoot", 2021);
+
+// Birds
+		Birds pigeon = new Birds("Pigeon", 1837);
+		Birds peacock = new Birds("Peacock", 1821);
+		Birds toucan = new Birds("Toucan", 1758);
+		Birds parrot = new Birds("Parrot", 1824);
+		Birds swan = new Birds("swan", 1758);
+
+// Fish
+		Fish salmon = new Fish("Salmon", 1758);
+		Fish catfish = new Fish("Catfish", 1817);
+		Fish perch = new Fish("Perch", 1758);
 	}
+
 }
